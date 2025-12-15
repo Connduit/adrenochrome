b6c2cd1c7f7f37f9971d2cd4dfd2ce0fc5abc1ce
connor <fuxkvalve@gmail.com>
2025-12-15 18:32:04 -0500

    separate funcs

--- loader/ReflectiveLoader.h
+++ loader/ReflectiveLoader.h
@@ -5,6 +5,7 @@
 #define ADRENOCHROME_REFLECTIVE_LOADER_H
 
 
+#include <minwindef.h>
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
@@ -26,7 +27,39 @@ typedef LPVOID (WINAPI* VIRTUALALLOC)
 typedef DWORD (NTAPI* NTFLUSHINSTRUCTIONCACHE)
 (HANDLE hProcess, PVOID lpBaseAddress, ULONG dwSize);
 
+//////////////////////////////////////////////////////////////
+// TODO: CONTEXTS
+typedef struct // TODO: needs a constructor to zero everything out?
+{
+	ULONG_PTR rawImageBase;
+	ULONG_PTR baseAddress;
+	PIMAGE_NT_HEADERS pNtHeaders;
+	LOADLIBRARYA pLoadLibraryA;
+	GETPROCADDRESS pGetProcAddress;
+	VIRTUALALLOC pVirtualAlloc;
+    NTFLUSHINSTRUCTIONCACHE pNtFlushInstructionCache;
+	//PVOID pNtdllBase; // TODO: 
+} LOADER_CONTEXT, *PLOADER_CONTEXT;
+
 //DLLEXPORT ULONG_PTR WINAPI ReflectiveLoader(LPVOID lpReserved);
 DLLEXPORT DWORD WINAPI ReflectiveLoader(LPVOID lpReserved);
+
+// TODO: 
+void loadModule(void); // load module into memory
+//ULONG_PTR getImageBase(void); // TODO: rename to initalizeLoader/initalizeLoaderContext? populate context...
+BOOL initializeContext(PLOADER_CONTEXT ctx); 
+DWORD resolveDependencies(PLOADER_CONTEXT ctx);
+BOOL copyImageIntoMemory(PLOADER_CONTEXT ctx); // TODO: rename to loadImageIntoMemory() ?
+BOOL applyRelocations(PLOADER_CONTEXT ctx); 
+BOOL resolveImports(PLOADER_CONTEXT ctx);
+// void handleTLS(void); // void handleTLSCallbacks(void);
+// void setProtections(void); // ?
+// void callEntryPoint(void);
+////////////////////////////////////////
+// getImports();
+// getExports();
+// parse();
+
+
 
 #endif
